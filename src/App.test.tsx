75d589d8bd4e8408c05ccd3c4291e46c85beca28
Rafael Castro <rafael.castro@seventh.com.br>
2022-05-31 13:49:12 -0300

    test pre-commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  render as renderRTL,
+  RenderOptions,
+  screen,
+} from "@testing-library/react";
 
-test('renders learn react link', () => {
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const AllTheProviders = ({ children }: { children: JSX.Element }) => {
+  return <BrowserRouter>{children}</BrowserRouter>;
+};
+
+const render = (
+  ui: JSX.Element,
+  options?:
+    | RenderOptions<
+        typeof import("@testing-library/dom/types/queries"),
+        HTMLElement,
+        HTMLElement
+      >
+    | undefined
+) => {
+  renderRTL(ui, { wrapper: AllTheProviders, ...options });
+};
+
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Hello world/i);
+  expect(title).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2 className="text-lg">Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about" className="text-lime-600">
+          About
+        </Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2 className="text-lg ">Who are we?</h2>
+        <p>That feels like an existential question, dont you think?</p>
+      </main>
+      <nav>
+        <Link to="/" className="text-lime-600">
+          Home
+        </Link>
+      </nav>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1 className="text-3xl font-bold underline text-red-700">
+          Hello world!
+        </h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
       </header>
     </div>
   );
